85f345366a06d1c1d08e96c2166c5d347c1ad888
moribon50 <moribon50@gmail.com>
2024-02-01 16:54:05 +0900

    Upgrade form

--- app/controllers/lends_controller.rb
+++ app/controllers/lends_controller.rb
@@ -1,4 +1,6 @@
 class LendsController < ApplicationController
+  before_action :authenticate_user!
+
   def index
   end
 
@@ -7,11 +9,11 @@ class LendsController < ApplicationController
   end
 
   def create
-    user = User.find_by(email: params[:email].downcase)
+    user = User.find_by(email: params[:loan][:email])
     if user
       logger.debug("if文の中に入りました")
-      @loan = Loan.new(lend_user: , borrow_user: , amount: , comment: ,return_on: )
-      
+      @loan = Loan.new(lend_user: current_user, borrow_user: user, amount: params[:loan][:amount], comment: params[:loan][:comment], return_on: params[:loan][:return_on])
+      logger.debug(@loan.lend_user_id)
     else
       logger.debug("else文の中に入りました")
       render 'new', status: :unprocessable_entity
