5def969532bb1404be8f20976ba1b2b4ef72373e
kodai maeda <isleep4070@icloud.com>
2024-02-01 10:09:25 +0900

    seedsの追加

--- app/models/loan.rb
+++ app/models/loan.rb
@@ -1,5 +1,5 @@
 class Loan < ApplicationRecord
   belongs_to :lend_user, class_name: "User"
   belongs_to :borrow_user, class_name: "User"
-  has_many :repayments
+  has_many :repayments, dependent: :destroy
 end
