cca59432d470f54c7e5eee369967a03f8b2ceaf6
RohanKid <tomas.s101515@gmail.com>
2024-02-02 08:27:47 +0000

    残高計算のロジック追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+    private
+    
+    def calcurateBalance(loan)
+     balance = loan.amount
+     loan.repayments.each do |repayment|
+       balance -= repayment.amount
+     end
+     return balance
+   end
 end
