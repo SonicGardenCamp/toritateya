421b999e288962ab4413e7ddeaad008a318c19cf
moribon50 <moribon50@gmail.com>
2024-02-02 11:44:23 +0900

    Add validation

--- app/models/loan.rb
+++ app/models/loan.rb
@@ -2,4 +2,7 @@ class Loan < ApplicationRecord
   belongs_to :lend_user, class_name: "User"
   belongs_to :borrow_user, class_name: "User"
   has_many :repayments, dependent: :destroy
+
+  validates :amount, presence: true
+  validates :return_on, presence: true
 end
